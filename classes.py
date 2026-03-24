b8c3ad81921ecb9f673de5321f14c82987291012
SergeyNote <sergei.kachilov@yandex.ru>
2026-03-24 15:18:54 +0500

    started django project

--- classes.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class CreateProduct(BaseModel):
-    name: str
-    sale: int = Field(ge=0)
-
-
--- seed.py
+++
@@ -1,7 +0,0 @@
-import db, models
-
-db.create_db_and_tables()
-
-with db.Session(db.engine) as s:
-    s.add(models.Hero(name="My hero", secret_name="her0"))
-    s.commit()
