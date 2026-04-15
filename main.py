35af2cc64a9237e6f2b67caf6e88de572a33ab8e
SergeyNote <sergei.kachilov@yandex.ru>
2026-04-15 09:07:47 +0500

    work with security

--- main.py
+++ main.py
@@ -1,121 +1,24 @@
-from fastapi import FastAPI
-# from classes import CreateProduct
-from typing import Annotated
-from models import *
-from db import create_db_and_tables, SessionDep
-
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, Security, HTTPException, Depends
+from fastapi.security import APIKeyHeader, HTTPBasic, HTTPBasicCredentials
 from sqlmodel import select
+from db import SessionDep
+from models import *
 
 app = FastAPI()
+api_key = APIKeyHeader(name="x-api-key")
+sec = HTTPBasic()
 
+@app.get("/test")
+def test1(api_token=Security(api_key)):
+    if api_token != "abc123":
+        raise HTTPException(403, "Неверный токен")
+    return {"secret": "ВАЖНО"}
 
-count = 0
-data = []
+@app.get("/test1")
+def test2(session: SessionDep, creds:HTTPBasicCredentials=Depends(sec)):
 
-@app.get("/product", response_model=list[Product])
-def GetProducts(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Product]:
-    heroes = session.exec(select(Product).offset(offset).limit(limit)).all()
-    return heroes
-
-@app.get("/product/{product_id}", response_model=Product)
-def GetProduct(product_id: int, session: SessionDep) -> Product:
-    product = session.get(Product, product_id)
-    if not product:
-        raise HTTPException(status_code=404, detail="product not found")
-    return product
-
-@app.post("/product", response_model=CreateProduct)
-def AddProduct(product: CreateProduct, session: SessionDep):
-    new_product = Product(name= product.name, description=product.description, price=product.price, category_id=product.category_id)
-    session.add(new_product)
-    session.commit()
-    # session.refresh(product)
-    return product
-
-@app.delete("/product")
-def DeleteProduct(product_id: int, session: SessionDep):
-    product = session.get(Product, product_id)
-    if not product:
-        raise HTTPException(status_code=404, detail="Product not found")
-    session.delete(product)
-    session.commit()
-    return {"ok": True}
-
-@app.patch("/product", response_model=CreateProduct)
-def UpdateProduct(product: UpdateProduct, session: SessionDep):
-    old_product = session.get(Product, product.id)
-    if not old_product:
-        raise HTTPException(status_code=404, detail="Product not found")
-
-    if product.name != None:
-        old_product.name = product.name
-
-    if product.description != None:
-        old_product.description = product.description
-
-    if product.price != None:
-        old_product.price = product.price
-
-    if product.category_id != None:
-        old_product.category_id = product.category_id
-    
-    session.add(old_product)
-    session.commit()
-    session.refresh(old_product)
-    return old_product
-
-@app.get("/category", response_model=list[Category])
-def GetCategories(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Category]:
-    categories = session.exec(select(Category).offset(offset).limit(limit)).all()
-    return categories
-
-@app.get("/category/{category_id}", response_model=Category)
-def GetCategory(category_id: int, session: SessionDep) -> Category:
-    category = session.get(Category, category_id)
-    if not category:
-        raise HTTPException(status_code=404, detail="category not found")
-    return category
-
-@app.post("/category", response_model=CreateCategory)
-def AddCategory(category: CreateCategory, session: SessionDep):
-    # categories = select(Category).where(Category.name == category.name)
-    # result = session.exec(categories)
-    # if result:
-    #     raise HTTPException(400, "category is already exist")
-    new_category = Category(name=category.name)
-    session.add(new_category)
-    session.commit()
-    # session.refresh(category)
-    return category
-
-@app.delete("/category")
-def DeleteCategory(category_id: int, session: SessionDep):
-    category = session.get(Category, category_id)
-    if not category:
-        raise HTTPException(status_code=404, detail="Category not found")
-    session.delete(category)
-    session.commit()
-    return {"ok": True}
-
-@app.patch("/category", response_model=CreateCategory)
-def UpdateCategory(category: UpdateCategory, session: SessionDep):
-    old_category = session.get(Category, category.id)
-    if not old_category:
-        raise HTTPException(status_code=404, detail="Category not found")
-
-    if category.name != None:
-        old_category.name = category.name
-    
-    session.add(old_category)
-    session.commit()
-    session.refresh(old_category)
-    return old_category
+    user = select(User).where(User.login == creds.username).where(User.password==creds.password)
+    result = session.exec(user).first()
+    if not result:
+        raise HTTPException(403, "Неверный логин или пароль")
+    return {"secret": "ВАЖНО"}
--- models.py
+++ models.py
@@ -9,37 +9,42 @@ from pydantic import Field as PydField
 #     secret_name: str
 #     age: int | None = None
 
-class Category(SQLModel, table=True):
+class User(SQLModel, table=True):
     id: int | None = SqlField(default=None, primary_key=True)
-    name: str
-    products: list["Product"] = Relationship(back_populates="category", cascade_delete=True)
+    login: str
+    password: str
 
-class Product(SQLModel, table=True):
-    id: int | None = SqlField(default=None, primary_key=True)
-    name: str
-    description: str | None = SqlField(default=None)
-    price: float
-    category_id: int = SqlField(foreign_key="category.id")
-    category: Category = Relationship(back_populates="products")
+# class Category(SQLModel, table=True):
+#     id: int | None = SqlField(default=None, primary_key=True)
+#     name: str
+#     products: list["Product"] = Relationship(back_populates="category", cascade_delete=True)
 
-class CreateProduct(BaseModel):
-    name: str
-    description: str | None = PydField(default=None)
-    price: float
-    category_id: int
+# class Product(SQLModel, table=True):
+#     id: int | None = SqlField(default=None, primary_key=True)
+#     name: str
+#     description: str | None = SqlField(default=None)
+#     price: float
+#     category_id: int = SqlField(foreign_key="category.id")
+#     category: Category = Relationship(back_populates="products")
 
-class CreateCategory(BaseModel):
-    name: str
+# class CreateProduct(BaseModel):
+#     name: str
+#     description: str | None = PydField(default=None)
+#     price: float
+#     category_id: int
 
-class UpdateCategory(BaseModel):
-    id: int = PydField(description="id изменяемой категории")
-    name: str | None = PydField(default=None)
-    # products: list["Product"] = Relationship(back_populates="category")
+# class CreateCategory(BaseModel):
+#     name: str
 
-class UpdateProduct(BaseModel):
-    id: int = PydField(description="id изменяемого продукта")
-    name: str | None = PydField(default=None)
-    description: str | None = PydField(default=None)
-    price: float | None = PydField(default=None)
-    category_id: int | None= PydField(foreign_key="category.id", default=None)
-    # category: Category = Relationship(back_populates="products")
+# class UpdateCategory(BaseModel):
+#     id: int = PydField(description="id изменяемой категории")
+#     name: str | None = PydField(default=None)
+#     # products: list["Product"] = Relationship(back_populates="category")
+
+# class UpdateProduct(BaseModel):
+#     id: int = PydField(description="id изменяемого продукта")
+#     name: str | None = PydField(default=None)
+#     description: str | None = PydField(default=None)
+#     price: float | None = PydField(default=None)
+#     category_id: int | None= PydField(foreign_key="category.id", default=None)
+#     # category: Category = Relationship(back_populates="products")
