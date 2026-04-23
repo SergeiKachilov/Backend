5c32637d10c5986da5383ae48e7f53ab8fff785d
SergeyNote <sergei.kachilov@yandex.ru>
2026-04-23 13:58:29 +0500

    wirk with files

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 import routers
+import shutil
+from uuid import uuid4
 
 app = FastAPI()
 app.include_router(routers.task_router)
+
+@app.post("/load_img")
+def Load_img(file: UploadFile):
+    file_location = f"upload/{uuid4()}.{file.filename.split('.')[-1]}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+
+    return {"src": file_location, "old_name":file.filename}
