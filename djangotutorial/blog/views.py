37c3db8e235634660a53cc7921ad45ad342856cd
SergeyNote <sergei.kachilov@yandex.ru>
2026-05-19 23:33:26 +0500

    finished django02

--- djangotutorial/blog/views.py
+++ djangotutorial/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.template import loader
+from django.core.paginator import Paginator
 
 # Create your views here.
 from django.http import HttpResponse
@@ -8,9 +9,14 @@ from .models import Blog, Comment
 
 
 def index(request):
-    latest_blogs = Blog.objects.order_by("-pub_date")
+    latest_blogs = Blog.objects.order_by("-pub_date").all()
+    paginator = Paginator(latest_blogs, 6)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    # return render(request, "index.html", {"latest_blogs": page_obj})
     template = loader.get_template("blog/index.html")
-    context = {"latest_blogs": latest_blogs}
+    context = {"latest_blogs": page_obj}
     return HttpResponse(template.render(context, request))
 
 def detail(request, blog_id):
